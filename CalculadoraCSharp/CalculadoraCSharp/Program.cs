05510257b20b6ea6a3a0f710ecc63a9114a14010
lu.civita <analouisecotrim@gmail.com>
2022-07-18 10:16:52 -0300

    “[CalculadoraCSharp]“

---
+++ CalculadoraCSharp/CalculadoraCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CalculadoraCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+
+            Console.WriteLine("Informe o número para a tabuada: ");
+            
+            numero = Double.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++)
+                {
+                double total = numero * i;
+                Console.WriteLine( numero + " X " + i + " = " + total);
+
+                }
+        }
+    }
+}
